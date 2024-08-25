2f5259c2a044318c4c637b8efbd253b26f1c8a0f
ceptar <christoph.cerjan@gmail.com>
2024-08-25 18:51:46 +0000

    cc

--- site/components/common/Navbar/NavbarRoot.tsx
+++ site/components/common/Navbar/NavbarRoot.tsx
@@ -1,42 +1,42 @@
-import { FC, useState, useEffect, ReactNode } from 'react'
-import throttle from 'lodash.throttle'
-import cn from 'clsx'
-import { motion, useAnimation } from 'framer-motion'
-import s from './Navbar.module.css'
+import { FC, useState, useEffect, ReactNode } from 'react';
+import { useTheme } from 'next-themes';
+import throttle from 'lodash.throttle';
+import cn from 'clsx';
+import { motion } from 'framer-motion';
+import s from './Navbar.module.css';
 
 const NavbarRoot: FC<{ children?: ReactNode }> = ({ children }) => {
-  const [scrollY, setScrollY] = useState(0)
-  const controls = useAnimation()
+  const { theme } = useTheme(); // Use the hook to get the current theme
+  const [scrollY, setScrollY] = useState(0);
 
   useEffect(() => {
     const handleScroll = throttle(() => {
-      const { scrollTop } = document.documentElement
-      setScrollY(scrollTop)
-    }, 200)
+      const { scrollTop } = document.documentElement;
+      setScrollY(scrollTop);
+    }, 200);
 
-    document.addEventListener('scroll', handleScroll)
+    document.addEventListener('scroll', handleScroll);
     return () => {
-      document.removeEventListener('scroll', handleScroll)
-    }
-  }, [])
+      document.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
-  useEffect(() => {
-    // Animate background opacity based on scroll position
-    const bgOpacity = scrollY < 48 ? scrollY / 48 : 1
-    controls.start({ '--bg-opacity': bgOpacity }) // Adjust the opacity variable based on scroll position
-  }, [scrollY, controls])
+  const bgOpacity = scrollY < 48 ? scrollY / 48 : 1;
+
+  // Determine the background color based on the theme
+  const baseColor = theme === 'dark' ? '17, 17, 17' : '247, 247, 244'; // Dark or Light RGB
 
   return (
     <motion.div
       className={cn(s.root)}
-      style={{ '--bg-opacity': 0 }} // Start with transparent background
-      animate={controls}
-      initial={{ '--bg-opacity': 0 }}
-      transition={{ duration: 0.2 }} // Smooth transition
+      style={{
+        backgroundColor: `rgba(${baseColor}, ${bgOpacity})`,
+        transition: 'background-color 0.3s',
+      }}
     >
       {children}
     </motion.div>
-  )
-}
+  );
+};
 
-export default NavbarRoot
+export default NavbarRoot;
